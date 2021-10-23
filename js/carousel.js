993f989e2a140931962073607cdd77963fe372be
PauloWarren <nagatogts@hotmail.com>
2021-10-23 19:07:26 -0300

    :art: responsividade

--- js/carousel.js
+++ js/carousel.js
@@ -1,40 +1,12 @@
 const config = {
     type: 'carousel',
-    perView: 4.5,
-    autoplay: '2000',
-    breakpoints: {
-        1069:{
-            perView: 3,
-        },
-        817:{
-            perView: 2,
-        },
-        535:{
-            perView: 1,
-        }
+    startAt: 1,
+    perView: 1,
+    autoplay: 3000,
+    align: 'center',
+    breakpoints:{
+
     }
 }
 
-
-const config2 = {
-    type: 'carousel',
-    perView: 1.4,
-    autoplay: '4000',
-}
-
-const config3 = {
-    type: 'carousel',
-    perView: 1,
-    autoplay: '4000',
-}
-
 new Glide('.glide', config).mount()
-
-new Glide('.glide2', config2).mount()
-//aa
-
-$("#Glide").glide({
-    type: "carousel",
-    autoplay: "5000"
-  });
-  
